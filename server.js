d99702b05cbf5d7f25e330c0412254c1709df5ab
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-09-22 22:11:05 -0700

    Setup html routes.

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.use(express.static('public'));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
+//Set up routes
+const path = require('path');
+
+app.get('/notes', (req, res) => {
+   res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
 app.listen(PORT, () => {
    console.log(`Server is listening on port ${PORT}`);
 });
